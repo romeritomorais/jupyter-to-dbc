3d7cc24958e87e4d10e078b3551f4f658f91ce7b
Romerito Morais <tr034803>
2022-02-09 23:42:36 -0300

    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx

--- sample.py
+++ sample.py
@@ -6,6 +6,7 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 dbutils = DBUtils(spark)
 
+
 print(dbutils.fs.ls("dbfs:/FileStore/packages"))
 
 # COMMAND ----------
