2bd579a053cc739d0f2418bbbc081be85e563d00
Romerito Morais <tr034803>
2022-02-09 22:44:45 -0300

    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx

---
+++ sample.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+
+from pyspark.dbutils import DBUtils
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+dbutils = DBUtils(spark)
+
+print(dbutils.fs.ls("dbfs:/FileStore/packages"))
